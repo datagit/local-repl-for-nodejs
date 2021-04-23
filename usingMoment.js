4984414c86f77588544d4b3025ba053cc2007d36
Dat Dao Man <dat.daoman@grevo.net>
2021-04-23 17:23:30 +0700

    a

--- usingMoment.js
+++ usingMoment.js
@@ -2,10 +2,7 @@ const moment = require('moment');
 
 // console.log(moment().endOf('day').utc());
 // console.log(moment().utcOffset('+0900').endOf('day').utc());
-const nailistId = 'x';
-const userViewNailistPage = undefined;
-if (userViewNailistPage && userViewNailistPage != nailistId) {
-  console.log('T');
-} else {
-  console.log('F');
-}
+const x = {z: new Date()};
+console.log(!!x.z);
+const obj = {z:'q'};
+console.log(!!obj.z);
