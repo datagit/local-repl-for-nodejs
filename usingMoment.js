47f92ac5ac24fbec67c1b6a7698d3789baf43706
Dat Dao Man <dat.daoman@grevo.net>
2021-04-27 10:32:53 +0700

    aa

--- usingMoment.js
+++ usingMoment.js
@@ -2,7 +2,28 @@ const moment = require('moment');
 
 // console.log(moment().endOf('day').utc());
 // console.log(moment().utcOffset('+0900').endOf('day').utc());
-const x = {z: new Date()};
+const x = { z: new Date() };
 console.log(!!x.z);
-const obj = {z:'q'};
+const obj = { z: 'q' };
 console.log(!!obj.z);
+
+const currentTime = moment().utc();
+const conditionDeleteMany = {
+  fromBeforeMonth: {
+    _created_at: { $lt: currentTime.clone().startOf('month').toDate() },
+  },
+  from2MonthAgo: {
+    _created_at: {
+      $lt: currentTime
+        .clone()
+        .startOf('month')
+        .subtract({ months: 2 })
+        .toDate(),
+    },
+  },
+};
+console.log(currentTime);
+console.log(currentTime.clone().startOf('month').toDate());
+console.log(
+  currentTime.clone().startOf('month').subtract({ months: 2 }).toDate()
+);
