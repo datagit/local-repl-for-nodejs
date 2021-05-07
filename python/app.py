49f35c05701a39e77951c6340f7bbc2204473a21
Dat Dao Man <dat.daoman@grevo.net>
2021-05-07 16:42:07 +0700

    add

---
+++ python/app.py
@@ -0,0 +1,15 @@
+import mymodule as mm
+mm.greeting('Dat Dao')
+print(mm.x(2, 3))
+
+
+import mymodule2 as mm2
+
+a = mm2.person1["age"]
+print(a)
+
+import numpy as ny
+print(ny.array({
+    "a1": 1,
+    "b1": 2,
+}))
---
+++ python/mymodule.py
@@ -0,0 +1,3 @@
+def greeting(name):
+  print("Hello, " + name)
+x = lambda a, b : a * b
